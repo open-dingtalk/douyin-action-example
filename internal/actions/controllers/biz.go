d4c356e95b89b1d66574d4cbf4015b61819fd32d
司沐 <ninan.nanni@alibaba-inc.com>
2024-01-03 21:17:08 +0800

    feat: 获取视频信息接口桥接Done

--- internal/actions/controllers/biz.go
+++ internal/actions/controllers/biz.go
@@ -6,7 +6,10 @@ import (
 	"encoding/json"
 	"github.com/gin-gonic/gin"
 	"io"
+	"log"
 	"net/http"
+	"net/url"
+	"strconv"
 	"strings"
 	"sync"
 )
@@ -17,6 +20,7 @@ import (
  **/
 
 const getUserInfoUrl string = "https://open.douyin.com/oauth/userinfo/"
+const getVideoListUrl string = "https://open.douyin.com/api/douyin/v1/video/video_list/"
 
 type BizController struct {
 	mu         sync.Mutex
@@ -93,7 +97,79 @@ func (bc *BizController) UserInfo(c *gin.Context) {
 }
 
 func (bc *BizController) GetVideoList(c *gin.Context) {
+	dyClient, err := NewDouYinClient()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, err)
+		return
+	}
 
+	accessToken := getBearerToken(c.Request)
+	getVideoListUrlWithParam := generateGetVideoListUrl(accessToken, 0, 10)
+
+	httpRequest, err := http.NewRequest("GET", getVideoListUrlWithParam, nil)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, err)
+		return
+	}
+	//httpRequest.Header.Set("Content-Type", "application/json")
+	httpRequest.Header.Set("access-token", accessToken)
+
+	httpResponse, err := dyClient.httpClient.Do(httpRequest)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, err)
+		return
+	}
+	defer httpResponse.Body.Close()
+	body, err := io.ReadAll(httpResponse.Body)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, err)
+		return
+	}
+
+	douYinTokenResponse := make(map[string]interface{})
+	if httpResponse.StatusCode == http.StatusOK {
+		if err := json.Unmarshal(body, &douYinTokenResponse); err != nil {
+			c.JSON(http.StatusInternalServerError, err)
+			return
+		}
+
+		respData := douYinTokenResponse["data"].(map[string]interface{})
+		respExtra := douYinTokenResponse["extra"].(map[string]interface{})
+		errorCode := respExtra["error_code"].(float64)
+		if errorCode == 0 {
+			getVideoListResponse := &models.GetVideoListResponse{}
+			videoList := respData["list"].([]interface{})
+			for _, rawVideo := range videoList {
+				// 类型断言，将interface{}转换为map[string]interface{}
+				video, ok := rawVideo.(map[string]interface{})
+				if !ok {
+					c.JSON(http.StatusInternalServerError, err)
+					return
+				}
+				videoItem := &models.VideoItem{}
+				videoItem.Title = video["title"].(string)
+
+				statistics := video["statistics"].(map[string]interface{})
+				videoItem.DiggCount = statistics["digg_count"].(float64)
+				videoItem.DownloadCount = statistics["download_count"].(float64)
+				videoItem.ShareCount = statistics["share_count"].(float64)
+				videoItem.ForwardCount = statistics["forward_count"].(float64)
+				videoItem.PlayCount = statistics["play_count"].(float64)
+
+				getVideoListResponse.Videos = append(getVideoListResponse.Videos, videoItem)
+			}
+			c.JSON(http.StatusOK, getVideoListResponse)
+		} else {
+			getTokenError := &models.DouYinError{}
+			getTokenError.ErrorCode = errorCode
+			getTokenError.ErrorDescription = respExtra["description"].(string)
+			c.JSON(http.StatusBadRequest, getTokenError)
+		}
+
+	} else {
+		c.JSON(http.StatusInternalServerError, err)
+		return
+	}
 }
 
 func (bc *BizController) GetVideoBase(c *gin.Context) {
@@ -118,4 +194,22 @@ func getBearerToken(r *http.Request) string {
 
 func getOpenID(accessToken string) string {
 	return "_000mWp_xZXvu3GStyvGh3QV4q3gM4dK8DPw"
+}
+
+func generateGetVideoListUrl(accessToken string, cursor int, count int) string {
+	// 解析URL，并确保没有错误发生
+	parsedURL, err := url.Parse(getVideoListUrl)
+	if err != nil {
+		log.Fatalf("Error parsing URL: %v", err)
+	}
+
+	// 创建一个URL值对象，用于添加query参数
+	parameters := url.Values{}
+	parameters.Add("open_id", getOpenID(accessToken))
+	parameters.Add("cursor", strconv.Itoa(cursor))
+	parameters.Add("count", strconv.Itoa(count))
+
+	// 将query参数添加到URL中
+	parsedURL.RawQuery = parameters.Encode()
+	return parsedURL.String()
 }
--- internal/actions/models/biz.go
+++ internal/actions/models/biz.go
@@ -16,3 +16,24 @@ type GetUserInfoResponse struct {
 	OpenID    string `json:"openId"`
 	UnionID   string `json:"unionId"`
 }
+
+type GetVideoListResponse struct {
+	Videos []*VideoItem
+}
+
+type VideoItem struct {
+	// 视频标题
+	Title string `json:"title"`
+	// 点赞数
+	DiggCount float64 `json:"diggCount"`
+	// 下载数
+	DownloadCount float64 `json:"downloadCount"`
+	// 播放数，只有作者本人可见。公开视频设为私密后，播放数也会返回0
+	PlayCount float64 `json:"playCount"`
+	// 分享数
+	ShareCount float64 `json:"shareCount"`
+	// 转发数
+	ForwardCount float64 `json:"forwardCount"`
+	// 评论数
+	CommentCount float64 `json:"CommentCount"`
+}
