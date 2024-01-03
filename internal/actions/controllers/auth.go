61f83045e247454054fa44525b5e8414354960f3
司沐 <ninan.nanni@alibaba-inc.com>
2024-01-03 17:36:25 +0800

    feat:token接口通过

--- internal/actions/controllers/auth.go
+++ internal/actions/controllers/auth.go
@@ -1,6 +1,8 @@
 package controllers
 
 import (
+	"bytes"
+	"context"
 	"douyin-action-example/internal/actions/models"
 	"douyin-action-example/internal/conf"
 	"encoding/json"
@@ -8,20 +10,41 @@ import (
 	"github.com/chzealot/gobase/logger"
 	"github.com/chzealot/gobase/utils"
 	"github.com/gin-gonic/gin"
+	"io"
+	"net"
 	"net/http"
 	"net/url"
 	"strings"
 	"sync"
+	"time"
 )
 
-const getTokenUrl string = "https://open.douyin.com/oauth/access_token"
+const getTokenUrl string = "https://open.douyin.com/oauth/access_token/"
 
 type AuthController struct {
-	mu sync.Mutex
+	mu         sync.Mutex
+	httpClient *http.Client
 }
 
 func NewAuthController() *AuthController {
-	return &AuthController{}
+	httpClient := &http.Client{
+		Transport: &http.Transport{
+			Proxy: http.ProxyFromEnvironment,
+			DialContext: func(ctx context.Context, _, addr string) (net.Conn, error) {
+				dialer := net.Dialer{}
+				return dialer.DialContext(ctx, "tcp", addr)
+			},
+			MaxIdleConns:          100,
+			IdleConnTimeout:       90 * time.Second,
+			TLSHandshakeTimeout:   10 * time.Second,
+			ExpectContinueTimeout: 60 * time.Second,
+			ResponseHeaderTimeout: 60 * time.Second,
+		},
+	}
+
+	return &AuthController{
+		httpClient: httpClient,
+	}
 }
 
 func (h AuthController) Authorize(c *gin.Context) {
@@ -76,35 +99,68 @@ func (h *AuthController) Callback(c *gin.Context) {
 }
 
 func (h *AuthController) Token(c *gin.Context) {
-	douYinClient, err := NewDouYinClient()
+	var getTokenRequest models.GetTokenRequest
+	err := json.NewDecoder(c.Request.Body).Decode(&getTokenRequest)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, err)
 		return
 	}
 
-	// Declare a new Person struct.
-	var tokenRequest models.GetTokenRequest
+	douYinTokenRequest := &models.DouYinGetTokenRequest{
+		ClientKey:    getTokenRequest.ClientID,
+		ClientSecret: getTokenRequest.ClientSecret,
+		Code:         getTokenRequest.Code,
+		GrantType:    getTokenRequest.GrantType,
+	}
+	jsonData, err := json.Marshal(douYinTokenRequest)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, err)
+		return
+	}
+	httpRequest, err := http.NewRequest("POST", getTokenUrl, bytes.NewBuffer(jsonData))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, err)
+		return
+	}
+	httpRequest.Header.Set("Content-Type", "application/json")
 
-	// Try to decode the request body into the struct. If there is an error,
-	// respond to the client with the error message and a 400 status code.
-	err = json.NewDecoder(c.Request.Body).Decode(&tokenRequest)
+	httpResponse, err := h.httpClient.Do(httpRequest)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, err)
+		return
+	}
+	defer httpResponse.Body.Close()
+	body, err := io.ReadAll(httpResponse.Body)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, err)
 		return
 	}
 
-	fmt.Printf("tokenRequest: %+v\n", tokenRequest)
+	douYinTokenResponse := make(map[string]interface{})
+	if httpResponse.StatusCode == http.StatusOK {
+		if err := json.Unmarshal(body, &douYinTokenResponse); err != nil {
+			c.JSON(http.StatusInternalServerError, err)
+			return
+		}
 
-	getTokenResponse := &models.GetTokenResponse{}
-	if err := douYinClient.Post(c, getTokenUrl, tokenRequest, getTokenResponse); err != nil {
-		c.Error(err)
+		respData := douYinTokenResponse["data"].(map[string]interface{})
+		errorCode := respData["error_code"].(float64)
+		if errorCode == 0 {
+			getTokenResponse := &models.GetTokenResponse{}
+			getTokenResponse.TokenType = "bearer"
+			getTokenResponse.AccessToken = respData["access_token"].(string)
+			getTokenResponse.RefreshToken = respData["refresh_token"].(string)
+			getTokenResponse.ExpireIn = int(respData["expires_in"].(float64))
+			c.JSON(http.StatusOK, getTokenResponse)
+		} else {
+			getTokenError := &models.GetTokenError{}
+			getTokenError.ErrorCode = errorCode
+			getTokenError.ErrorDescription = respData["description"].(string)
+			c.JSON(http.StatusBadRequest, getTokenError)
+		}
+
+	} else {
+		c.JSON(http.StatusInternalServerError, err)
 		return
 	}
-
-	resp := make(map[string]any)
-	resp["access_token"] = getTokenResponse.AccessToken
-	resp["token_type"] = "bearer"
-	resp["refresh_token"] = getTokenResponse.RefreshToken
-	resp["expires_in"] = getTokenResponse.ExpireIn
-	c.JSON(http.StatusOK, resp)
 }
--- internal/actions/models/oauth.go
+++ internal/actions/models/oauth.go
@@ -12,8 +12,21 @@ type GetTokenRequest struct {
 	GrantType    string `json:"grant_type"`
 }
 
+type DouYinGetTokenRequest struct {
+	ClientKey    string `json:"client_key"`
+	ClientSecret string `json:"client_secret"`
+	Code         string `json:"code"`
+	GrantType    string `json:"grant_type"`
+}
+
 type GetTokenResponse struct {
+	TokenType    string `json:"token_type"`
 	AccessToken  string `json:"access_token"`
 	RefreshToken string `json:"refresh_token"`
 	ExpireIn     int    `json:"expires_in"`
+}
+
+type GetTokenError struct {
+	ErrorCode        float64 `json:"error_code"`
+	ErrorDescription string  `json:"error_description"`
 }
